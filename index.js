48110072ee0b01227f7d201bb328d837964e5b96
HelioStrike <rageofepicfury@gmail.com>
2019-03-22 12:15:46 +0530

    disabled yellow boxes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+console.disableYellowBox = true;
+
 var config = {
     apiKey: "AIzaSyBUq-Ljxz1dATPVN9hsQ04FPQIsbkKE20k",
     authDomain: "zigger-fab73.firebaseapp.com",
